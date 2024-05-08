410552b7c0fbe105f47770fae53e42b38e532807
Farisbahdlor <39289628+Farisbahdlor@users.noreply.github.com>
2024-05-08 14:46:35 +0700

    Update LendingProtocol.sol

--- LendingProtocol.sol
+++ LendingProtocol.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 
 interface ILendingProtocol {
     function borrow(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Value, uint256 _DueTime) external returns (bool);
+    function addCollateral(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _AmountAdd) external returns (bool);
+    function decreaseCollateral(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _AmountDecrease) external returns (bool);
+    function LTVCheck(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _Value) external view returns (uint256);
+    
+
     event LoanApproval (address _Collateral, address _Borrow, uint256 _CollateralAmount , uint256 _LoanAmount, uint256 Timestamp, uint256 DueTime);
 }
 
@@ -27,8 +32,8 @@ interface IERC20Vault {
 contract LendingProtocol is ILendingProtocol {
 
     struct LendingData {
-        uint256 collateral;
-        uint256 InitialLTV;
+        uint256 Collateral;
+        uint256 LTV;
         uint256 LoanAmount;
         uint256 LoanInterest;
         uint256 StartTime;
@@ -42,6 +47,7 @@ contract LendingProtocol is ILendingProtocol {
 
     //Loan to Value Ratio
     mapping(address => mapping(address => uint256)) public LoanToValue;
+    mapping(address => mapping(address => uint256)) public LiquidationRatio;
     uint256 LoanFee;
 
     address Owner;
@@ -88,6 +94,46 @@ contract LendingProtocol is ILendingProtocol {
 
         return true;
     }
+    
 
+    function addCollateral(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _AmountAdd) external override returns (bool){
+        //Borrower must approve first in main contract to execute this function.
+        require(IERC20Vault (_CollateralAsset).allowance(_Borrower, address(this)) >= _AmountAdd);
+        //Transfer collateral asset from user to vault based on loan requested using approval and transferfrom method.
+        
+        uint256 _NewCollateral = Lending[_CollateralAsset][_BorrowAsset][_Borrower][_Index].Collateral + _AmountAdd;
+        uint256 _NewLTV = LTVCheck(_CollateralAsset, _Borrower, _BorrowAsset, _Index, _NewCollateral);
+        require(_NewLTV >= LiquidationRatio[_CollateralAsset][_BorrowAsset], "New LTV must lower then liquidation ratio");
+        require(IERC20Vault (_CollateralAsset).transferFrom(_Borrower, _CollateralAsset, _AmountAdd), "Transfer From new added collateral failed.");
+        require(loanToValueUpdate(_CollateralAsset, _Borrower, _BorrowAsset, _Index, _NewCollateral, _NewLTV), "LTV update failed");
+
+        return true;
+    }
+
+    function decreaseCollateral(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _AmountDecrease) external override returns (bool){
+        uint256 _NewCollateral = Lending[_CollateralAsset][_BorrowAsset][_Borrower][_Index].Collateral + _AmountDecrease;
+        uint256 _NewLTV = LTVCheck(_CollateralAsset, _Borrower, _BorrowAsset, _Index, _NewCollateral);
+        require(_NewLTV >= LiquidationRatio[_CollateralAsset][_BorrowAsset], "New LTV must lower then liquidation ratio");
+        require(IERC20Vault (_CollateralAsset).transfer(_CollateralAsset, _Borrower, _AmountDecrease), "Transfer amount collateral decrease failed");
+        require(loanToValueUpdate(_CollateralAsset, _Borrower, _BorrowAsset, _Index, _NewCollateral, _NewLTV), "LTV update failed");
+
+        return true;
+    }
+    function loanToValueUpdate(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _NewCollateralValue, uint256 _NewLTV) internal returns (bool){
+        Lending[_CollateralAsset][_BorrowAsset][_Borrower][_Index].LTV = _NewLTV;
+        Lending[_CollateralAsset][_BorrowAsset][_Borrower][_Index].Collateral = _NewCollateralValue;
+        return true;
+    }
+
+    //LTV check public to external
+    function LTVCheck(address _CollateralAsset, address _Borrower, address _BorrowAsset, uint256 _Index, uint256 _Value) public view override returns (uint256){
+        // uint256 _LoanAmount = Lending[_CollateralAsset][_BorrowAsset][_Borrower].LoanAmount;
+        uint256 _NewLTV = Lending[_CollateralAsset][_BorrowAsset][_Borrower][_Index].LoanAmount / loanAmount(_CollateralAsset, _BorrowAsset, _Value) * 100;
+        return _NewLTV;
+    }
+
+    function paybackLoan(address _CollateralAsset, address _Borrower, address _BorrowAsset) external returns (bool){
+
+    }
 
 }
